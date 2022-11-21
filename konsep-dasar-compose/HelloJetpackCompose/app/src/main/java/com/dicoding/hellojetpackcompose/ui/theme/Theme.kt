adc07eabfce267de8ea56af484b5fe882f15e036
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-11-21 14:54:03 +0700

    Update library and Compose version to 1.3.1

--- konsep-dasar-compose/HelloJetpackCompose/app/src/main/java/com/dicoding/hellojetpackcompose/ui/theme/Theme.kt
+++ konsep-dasar-compose/HelloJetpackCompose/app/src/main/java/com/dicoding/hellojetpackcompose/ui/theme/Theme.kt
@@ -5,8 +5,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Green
 
 private val DarkColorPalette = darkColors(
     primary = Navy,
