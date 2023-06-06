6491780cd7b117a5cdad716b18e834f054b5cbed
Arif <faizin.arief@gmail.com>
2023-06-06 11:10:20 +0700

    Fix theming color

--- layout-pada-compose/JetCoffee/app/src/main/java/com/dicoding/jetcoffee/ui/theme/Color.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/dicoding/jetcoffee/ui/theme/Color.kt
@@ -12,4 +12,3 @@ val Pink40 = Color(0xFF7D5260)
 
 val CoffeeBrown = Color(0xFF886345)
 val LightCoffeeBrown = Color(0xFFC4A68A)
-val LightGray = Color(0xFFBBBBBB)
--- layout-pada-compose/JetCoffee/app/src/main/java/com/dicoding/jetcoffee/ui/theme/Theme.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/dicoding/jetcoffee/ui/theme/Theme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = LightCoffeeBrown,
+    secondary = LightCoffeeBrown,
     tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey80,
+    primary = CoffeeBrown,
+    secondary = CoffeeBrown,
+    secondaryContainer = LightCoffeeBrown,
     tertiary = Pink40,
+    background = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +44,7 @@ private val LightColorScheme = lightColorScheme(
 fun JetCoffeeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
