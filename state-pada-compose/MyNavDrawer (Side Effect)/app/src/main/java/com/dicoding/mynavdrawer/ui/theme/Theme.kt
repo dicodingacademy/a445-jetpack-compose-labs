de6a5636ffb43dac88a99b2e7b90a72ceb805f2a
Arif <faizin.arief@gmail.com>
2023-06-07 15:19:19 +0700

    Add monochrome

--- state-pada-compose/MyNavDrawer (Side Effect)/app/src/main/java/com/dicoding/mynavdrawer/ui/theme/Theme.kt
+++ state-pada-compose/MyNavDrawer (Side Effect)/app/src/main/java/com/dicoding/mynavdrawer/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun MyNavDrawerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -49,6 +49,7 @@ fun MyNavDrawerTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
