86fdf6629d0e28b18ed7e26a6cd033a672e200af
Arif <faizin.arief@gmail.com>
2023-06-08 10:59:21 +0700

    Update AGP 8 & M3

--- state-pada-compose/MyNavDrawer (State Holder)/app/src/main/java/com/dicoding/mynavdrawer/MainActivity.kt
+++ state-pada-compose/MyNavDrawer (State Holder)/app/src/main/java/com/dicoding/mynavdrawer/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.dicoding.mynavdrawer.ui.theme.MyNavDrawerTheme
 
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     MyNavDrawerApp()
                 }
