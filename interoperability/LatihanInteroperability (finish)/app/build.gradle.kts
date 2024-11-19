37b8e7003c617fe6ba7283f489ec478c19494eb4
Arif <faizin.arief@gmail.com>
2024-11-19 15:04:45 +0700

    Update KTS Latihan Interoperability (finish) like starter

--- interoperability/LatihanInteroperability (finish)/app/build.gradle.kts
+++ interoperability/LatihanInteroperability (finish)/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.dicoding.newsapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
