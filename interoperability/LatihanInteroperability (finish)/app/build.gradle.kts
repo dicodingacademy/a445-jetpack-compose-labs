163d17625f5c5ecda34f02466401d02aadd76305
Arif <faizin.arief@gmail.com>
2024-11-21 11:27:32 +0700

    clean up

--- interoperability/LatihanInteroperability (finish)/app/build.gradle.kts
+++ interoperability/LatihanInteroperability (finish)/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.dicoding.newsapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
