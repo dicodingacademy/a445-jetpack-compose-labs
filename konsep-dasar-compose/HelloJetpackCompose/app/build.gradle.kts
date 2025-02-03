76730134b739902dfc6f7099a28e655ca8a5b9a6
Arif <faizin.arief@gmail.com>
2025-02-03 13:23:09 +0700

    Update AGP 8.8 + add material icon extended

--- konsep-dasar-compose/HelloJetpackCompose/app/build.gradle.kts
+++ konsep-dasar-compose/HelloJetpackCompose/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
