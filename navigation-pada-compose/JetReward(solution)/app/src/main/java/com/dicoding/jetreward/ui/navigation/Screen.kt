5a6d4ecdd9c93a685c82f520895a531c8bac43ad
Arif <faizin.arief@gmail.com>
2025-02-20 10:37:35 +0700

    Change object to data object

--- navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/navigation/Screen.kt
+++ navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/navigation/Screen.kt
@@ -1,10 +1,10 @@
 package com.dicoding.jetreward.ui.navigation
 
 sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Cart : Screen("cart")
-    object Profile : Screen("profile")
-    object DetailReward : Screen("home/{rewardId}") {
+    data object Home : Screen("home")
+    data object Cart : Screen("cart")
+    data object Profile : Screen("profile")
+    data object DetailReward : Screen("home/{rewardId}") {
         fun createRoute(rewardId: Long) = "home/$rewardId"
     }
 }
