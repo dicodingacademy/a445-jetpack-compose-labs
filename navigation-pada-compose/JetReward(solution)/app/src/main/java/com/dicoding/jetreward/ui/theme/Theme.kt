3ebaa2207a9c292e5ad755456b2f76f5bbb6addf
Arif <faizin.arief@gmail.com>
2025-02-20 10:10:17 +0700

    Update to ladybug

--- navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/theme/Theme.kt
+++ navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/theme/Theme.kt
@@ -61,14 +61,6 @@ fun JetRewardTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
