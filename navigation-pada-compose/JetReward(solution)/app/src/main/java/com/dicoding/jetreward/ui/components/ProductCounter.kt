ef655bc531ef9e745680424800a8d2b1c53a04a1
Arif <faizin.arief@gmail.com>
2025-02-20 14:45:46 +0700

    Change counter using OutlinedIconButton

--- navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/components/ProductCounter.kt
+++ navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/components/ProductCounter.kt
@@ -1,18 +1,33 @@
 package com.dicoding.jetreward.ui.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -20,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
+import org.w3c.dom.Text
 
 @Composable
 fun ProductCounter(
@@ -32,26 +48,20 @@ fun ProductCounter(
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.size(width = 110.dp, height = 40.dp).padding(4.dp)
+        modifier = modifier
+            .size(width = 110.dp, height = 40.dp)
+            .padding(4.dp)
     ) {
-        Surface(
-            shape = RoundedCornerShape(size = 5.dp),
-            border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
-            color = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.size(30.dp)
-        ) {
-            Text(
-                text = "—",
-                fontSize = 22.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .weight(1f)
-                    .clickable {
-                        onProductDecreased(orderId)
-                    }
-            )
-        }
+        CounterButton(
+            text = "—",
+            modifier = Modifier
+                .weight(1f)
+                .size(30.dp)
+                .padding(1.dp),
+            onClick = {
+                onProductDecreased(orderId)
+            },
+        )
         Text(
             text = orderCount.toString(),
             modifier = Modifier
@@ -60,24 +70,38 @@ fun ProductCounter(
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold
         )
-        Surface(
-            shape = RoundedCornerShape(size = 5.dp),
-            border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
-            color = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.size(30.dp)
-        ) {
-            Text(
-                text = "＋",
-                fontSize = 22.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .weight(1f)
-                    .clickable {
-                        onProductIncreased(orderId)
-                    }
-            )
-        }
+        CounterButton(
+            text = "+",
+            modifier = Modifier
+                .weight(1f)
+                .size(30.dp)
+                .padding(1.dp),
+            onClick = {
+                onProductIncreased(orderId)
+            },
+        )
+    }
+}
+
+@Composable
+fun CounterButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    OutlinedIconButton(
+        onClick = {
+            onClick()
+        },
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        shape = RoundedCornerShape(size = 5.dp),
+        modifier = modifier
+    ) {
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            color = MaterialTheme.colorScheme.primary,
+        )
     }
 }
 
--- navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/screen/cart/CartScreen.kt
+++ navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/screen/cart/CartScreen.kt
@@ -1,10 +1,14 @@
 package com.dicoding.jetreward.ui.screen.cart
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
